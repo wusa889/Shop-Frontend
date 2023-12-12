46f88939f268caf8a6f23795f4dac3f1409ec556
wusa889 <144148191+wusa889@users.noreply.github.com>
2023-12-12 18:28:21 +0100

    Added page that lists all categories

--- src/app/pages/category/category-list/category-list.component.ts
+++ src/app/pages/category/category-list/category-list.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {CategoryControllerService} from "../../../openapi-client";
+import {CategoryControllerService, CategoryShowDto} from "../../../openapi-client";
+import {Observable} from "rxjs";
+import {MatTableModule} from "@angular/material/table";
 
 @Component({
   selector: 'pm-category-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.scss'
 })
 export class CategoryListComponent {
+  categories: CategoryShowDto[] = [];
+  displayedColumns: string[] = ['name']; // Add more column names as needed
+
   constructor(
     private readonly catService: CategoryControllerService
-  ) {
+  ) { }
+
+  ngOnInit(): void {
+    this.showCategories();
   }
-  showCategories(): void{
+
+  showCategories(): void {
     this.catService.getAllCategories().subscribe(value => {
-      console.log(value)
-    })
+      this.categories = value;
+    });
   }
 
 }
