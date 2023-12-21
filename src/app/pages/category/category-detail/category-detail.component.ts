4221f0d6bd9cb5ed530a1867bb1339f8b8855ee6
wusa889 <144148191+wusa889@users.noreply.github.com>
2023-12-21 17:47:32 +0100

    Configured detail pages for categories and products

--- src/app/pages/category/category-detail/category-detail.component.ts
+++ src/app/pages/category/category-detail/category-detail.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CategoryControllerService, CategoryDetailDto} from "../../../openapi-client";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTableModule} from "@angular/material/table";
 
 @Component({
   selector: 'pm-category-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatTableModule],
   templateUrl: './category-detail.component.html',
   styleUrl: './category-detail.component.scss'
 })
 export class CategoryDetailComponent {
+  categoryId?: number;
+  showCategory?: CategoryDetailDto;
+  displayData: any[] = [];
+  constructor(
+    private readonly catService: CategoryControllerService,
+    private router: Router,
+    private activeRouter: ActivatedRoute
+  ) {
+  }
+  ngOnInit(): void {
+    this.activeRouter.params.subscribe((params: Params) => {
+      const categoryId = +params['id']; // The '+' converts the string to a number
+      if (categoryId) {
+        this.categoryId = categoryId; // Set your component's categoryId property
+        this.getCategoryData(this.categoryId);
+      }
+    });
+  }
+  goBack(): void{
+    this.router.navigateByUrl('/category/all')
+  }
+  getCategoryData(catId: number): void {
+    this.catService.getCategoryById(catId).subscribe(value => {
+      this.showCategory = value;
+      this.transformCategoryData(value);
+    });
+  }
+
+  transformCategoryData(category: CategoryDetailDto) {
+    this.displayData = [
+      { key: 'Id:', value: category.id },
+      { key: 'Active:', value: category.active ? 'Yes' : 'No' },
+      { key: 'Name:', value: category.name },
+      { key: 'Products:', value: category.products.map(p => p.name).join(', ') }
+    ];
+  }
+
 
 }
--- src/app/pages/products/product-detail/product-detail.component.ts
+++ src/app/pages/products/product-detail/product-detail.component.ts
@@ -1,13 +1,68 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  CategoryControllerService,
+  CategoryDetailDto,
+  ProductControllerService,
+  ProductDetailDto
+} from "../../../openapi-client";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTableModule} from "@angular/material/table";
 
 @Component({
   selector: 'pm-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatTableModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent {
+  productId?: number;
+  showProduct?: ProductDetailDto;
+  displayData: any[] = [];
+  imageSource?: string
 
+  constructor(
+    private readonly prodService: ProductControllerService,
+    private router: Router,
+    private activeRouter: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.activeRouter.params.subscribe((params: Params) => {
+      const productId = +params['id']; // The '+' converts the string to a number
+      if (productId) {
+        this.productId = productId; // Set your component's categoryId property
+        this.getProductData(this.productId);
+      }
+    });
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('/product/all')
+  }
+
+  private getProductData(productId: number) {
+    this.prodService.getProductById(productId).subscribe(value => {
+      this.showProduct = value;
+      this.transformProductData(value);
+      this.imageSource = value.image
+    });
+  }
+
+
+  private transformProductData(product: ProductDetailDto) {
+    this.displayData = [
+      {key: 'Id', value: product.id},
+      {key: 'sku', value: product.sku},
+      {key: 'Name', value: product.name},
+      {key: 'description', value: product.active},
+      {key: 'Active', value: product.active ? 'Yes' : 'No' },
+      {key: 'price', value: product.price},
+      {key: 'stock', value: product.stock},
+      {key: 'category', value: product.category?.name},
+    ];
+  }
 }
